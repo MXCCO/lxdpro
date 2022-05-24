b119bf8cb44a4b59efa59d58abb11017247aad54
MXCCO <106053165+MXCCO@users.noreply.github.com>
2022-05-25 00:14:59 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,5 +1,6 @@
 apt-get install openssh-server -y
-sudo sed -i "s/^#\?Port.*/Port $1/g" /etc/ssh/sshd_config;
+port=$1
+sudo sed -i "s/^#\?Port.*/Port ${port}/g" /etc/ssh/sshd_config;
 sudo sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
 sudo sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
 service sshd restart
