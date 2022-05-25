ab725687f2ea1acc38aecc54b843a0c53afef2e9
MXCCO <106053165+MXCCO@users.noreply.github.com>
2022-05-25 13:54:30 +0800

    Delete root.sh

--- root.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install openssh-server -y
-port=$1
-sudo sed -i "s/^#\?Port.*/Port ${port}/g" /etc/ssh/sshd_config;
-sudo sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
-sudo sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
-service sshd restart
-systemctl enable sshd.service>/dev/null 2>&1
-passwd=$2
-echo root:$passwd | sudo chpasswd root
