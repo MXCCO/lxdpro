0da2627de64610bb4e31dad4423386237ad9c8c2
MXCCO <106053165+MXCCO@users.noreply.github.com>
2023-05-12 14:32:26 +0800

    Update lxdpro.sh

--- lxdpro.sh
+++ lxdpro.sh
@@ -15,7 +15,7 @@ Pe="\033[0;35m"
 #报错检测
 snap_detect(){
     lxd_lxc_detect=`lxc list`
-    if [[ "$lxd_lxc_detect" =~ command not found.* ]]
+    if [ $? -ne 0 ]
     then
     echo "lxd已安装但是无法使用，请尝试重启"
     exit 0
