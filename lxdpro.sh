829d7fafa35eb43d81d9fdbda1865a38192f5a12
MXCCO <106053165+MXCCO@users.noreply.github.com>
2023-05-12 14:18:05 +0800

    Update lxdpro.sh

--- lxdpro.sh
+++ lxdpro.sh
@@ -18,8 +18,7 @@ snap_detect(){
     if [[ "$lxd_lxc_detect" =~ "command not found".* ]]
     then
     echo "lxd已安装但是无法使用，请尝试重启"
-    exit 0
-    else
+    exit 
     if [[ "$lxd_lxc_detect" =~ "snap-update-ns failed with code1".* ]]
     then
     systemctl restart apparmor
